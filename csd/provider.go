09476ee17a5125af8251e4b3b0f652e79ef89e8b
Ricardo Band <ricardo.band@idealo.de>
2023-02-07 13:18:00 +0100

    prepare first release

--- csd/provider.go
+++ csd/provider.go
@@ -2,95 +2,123 @@ package csd
 
 import (
 	"context"
+	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-func Provider() *schema.Provider {
-	return &schema.Provider{
-		// Configure terraform provider with AWS credentials
-		Schema: map[string]*schema.Schema{
-			"aws_access_key_id": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Sensitive:   true,
-				DefaultFunc: schema.EnvDefaultFunc("AWS_ACCESS_KEY_ID", ""),
+func init() {
+	// Set descriptions to support markdown syntax, this will be used in document generation
+	// and the language server.
+	schema.DescriptionKind = schema.StringMarkdown
+
+	// Customize the content of descriptions when output. For example you can add defaults on
+	// to the exported descriptions if present.
+	// schema.SchemaDescriptionBuilder = func(s *schema.Schema) string {
+	// 	desc := s.Description
+	// 	if s.Default != nil {
+	// 		desc += fmt.Sprintf(" Defaults to `%v`.", s.Default)
+	// 	}
+	// 	return strings.TrimSpace(desc)
+	// }
+}
+
+func New(version string, commit string) func() *schema.Provider {
+	return func() *schema.Provider {
+		p := &schema.Provider{
+			// Configure terraform provider with AWS credentials
+			Schema: map[string]*schema.Schema{
+				"aws_access_key_id": {
+					Type:        schema.TypeString,
+					Required:    true,
+					Sensitive:   true,
+					DefaultFunc: schema.EnvDefaultFunc("AWS_ACCESS_KEY_ID", ""),
+				},
+				"aws_secret_access_key": {
+					Type:        schema.TypeString,
+					Required:    true,
+					Sensitive:   true,
+					DefaultFunc: schema.EnvDefaultFunc("AWS_SECRET_ACCESS_KEY", ""),
+				},
+				"aws_session_token": {
+					Type:        schema.TypeString,
+					Required:    true,
+					Sensitive:   true,
+					DefaultFunc: schema.EnvDefaultFunc("AWS_SESSION_TOKEN", ""),
+				},
 			},
-			"aws_secret_access_key": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Sensitive:   true,
-				DefaultFunc: schema.EnvDefaultFunc("AWS_SECRET_ACCESS_KEY", ""),
+			ResourcesMap: map[string]*schema.Resource{
+				"csd_zone": resourceZone(),
 			},
-			"aws_session_token": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Sensitive:   true,
-				DefaultFunc: schema.EnvDefaultFunc("AWS_SESSION_TOKEN", ""),
+			DataSourcesMap: map[string]*schema.Resource{
+				"csd_zones": dataSourceZones(),
+				"csd_zone":  dataSourceZone(),
 			},
-		},
-		ResourcesMap: map[string]*schema.Resource{
-			"csd_zone": resourceZone(),
-		},
-		DataSourcesMap: map[string]*schema.Resource{
-			"csd_zones": dataSourceZones(),
-			"csd_zone":  dataSourceZone(),
-		},
-		ConfigureContextFunc: providerConfigure,
+		}
+
+		p.ConfigureContextFunc = configure(version, commit, p)
+
+		return p
 	}
 }
 
-// providerConfigure Stores the AWS credentials from the provider configuration so later HTTP requests can use them
-func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
-	/*
-		AWS credentials will be grabbed from environment variables or from the terraform provider configuration like this:
-		provider "csd" {
-		  aws_access_key_id     = "superSecret123!"
-		  aws_secret_access_key = "superSecret123!"
-		  aws_session_token     = "superSecret123!"
+// configure Stores the AWS credentials from the provider configuration so later HTTP requests can use them
+func configure(version string, commit string, p *schema.Provider) func(context.Context, *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	return func(c context.Context, d *schema.ResourceData) (any, diag.Diagnostics) {
+		// Setup a User-Agent for the API client
+		userAgent := p.UserAgent("terraform-provider-csd", fmt.Sprintf("%s (%s)", version, commit))
+
+		/*
+			AWS credentials will be grabbed from environment variables or from the terraform provider configuration like this:
+			provider "csd" {
+			  aws_access_key_id     = "superSecret123!"
+			  aws_secret_access_key = "superSecret123!"
+			  aws_session_token     = "superSecret123!"
+			}
+		*/
+		awsAccessKeyId := d.Get("aws_access_key_id").(string)
+		awsSecretAccessKey := d.Get("aws_secret_access_key").(string)
+		awsSessionToken := d.Get("aws_session_token").(string)
+
+		// Warning or errors can be collected in a slice type
+		var diags diag.Diagnostics
+
+		// Check for the provided strings and create proper errors if they are missing
+		if awsAccessKeyId == "" {
+			diags = append(diags, diag.Diagnostic{
+				Severity: diag.Error,
+				Summary:  "Unable to find authentication info for AWS",
+				Detail:   "Value for aws_access_key_id is missing",
+			})
 		}
-	*/
-	awsAccessKeyId := d.Get("aws_access_key_id").(string)
-	awsSecretAccessKey := d.Get("aws_secret_access_key").(string)
-	awsSessionToken := d.Get("aws_session_token").(string)
+		if awsSecretAccessKey == "" {
+			diags = append(diags, diag.Diagnostic{
+				Severity: diag.Error,
+				Summary:  "Unable to find authentication info for AWS",
+				Detail:   "Value for aws_secret_access_key is missing",
+			})
+		}
+		if awsSessionToken == "" {
+			diags = append(diags, diag.Diagnostic{
+				Severity: diag.Error,
+				Summary:  "Unable to find authentication info for AWS",
+				Detail:   "Value for aws_session_token is missing",
+			})
+		}
 
-	// Warning or errors can be collected in a slice type
-	var diags diag.Diagnostics
+		// Create an API Client that holds the credentials and convenience function for HTTP communication
+		apiClient := ApiClient{
+			AccessKeyId:     awsAccessKeyId,
+			SecretAccessKey: awsSecretAccessKey,
+			SessionToken:    awsSessionToken,
+			UserAgent:       userAgent,
+		}
 
-	// Check for the provided strings and create proper errors if they are missing
-	if awsAccessKeyId == "" {
-		diags = append(diags, diag.Diagnostic{
-			Severity: diag.Error,
-			Summary:  "Unable to find authentication info for AWS",
-			Detail:   "Value for aws_access_key_id is missing",
-		})
-	}
-	if awsSecretAccessKey == "" {
-		diags = append(diags, diag.Diagnostic{
-			Severity: diag.Error,
-			Summary:  "Unable to find authentication info for AWS",
-			Detail:   "Value for aws_secret_access_key is missing",
-		})
-	}
-	if awsSessionToken == "" {
-		diags = append(diags, diag.Diagnostic{
-			Severity: diag.Error,
-			Summary:  "Unable to find authentication info for AWS",
-			Detail:   "Value for aws_session_token is missing",
-		})
-	}
+		// Test the connection to find out if credentials are valid and endpoint is working
+		if _, err := apiClient.getZones(); err != nil {
+			diags = append(diags, err...)
+		}
 
-	// Create an API Client that holds the credentials and convenience function for HTTP communication
-	apiClient := ApiClient{
-		AccessKeyId:     awsAccessKeyId,
-		SecretAccessKey: awsSecretAccessKey,
-		SessionToken:    awsSessionToken,
+		return &apiClient, diags
 	}
-
-	// Test the connection to find out if credentials are valid and endpoint is working
-	if _, err := apiClient.getZones(); err != nil {
-		diags = append(diags, err...)
-	}
-
-	return &apiClient, diags
 }
