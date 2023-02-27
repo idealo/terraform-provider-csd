fa073aa5f352e9be513d1bc2f4cdfda2068fbf24
Manuel Marquez <manuel.marquez@idealo.de>
2023-02-27 13:24:39 +0000

    Add support for AWS Profiles

--- examples/main.tf
+++ examples/main.tf
@@ -12,16 +12,17 @@ terraform {
   }
 }
 
-provider "aws" {
-  region              = "eu-central-1"
-  allowed_account_ids = ["433744410943"]
+#provider "aws" {
+ # region              = "eu-central-1"
+  #allowed_account_ids = ["433744410943"]
+#}
+
+provider "csd" {
 }
 
-provider "csd" {}
-
-resource "aws_route53_zone" "myzone" {
-  name = "myzone.idealo.tools"
-}
+#resource "aws_route53_zone" "myzone" {
+#  name = "myzone.idealo.tools"
+#}
 
 #resource "csd_zone" "myzone" {
 #  name = aws_route53_zone.myzone.name
@@ -36,11 +37,11 @@ resource "aws_route53_zone" "myzone" {
 #  value = csd_zone.myzone.name_servers
 #}
 
-#data "csd_zones" "all" {}
-#
-#output "test_data_read_zones" {
-#  value = data.csd_zones.all
-#}
+data "csd_zones" "all" {}
+
+output "test_data_read_zones" {
+  value = data.csd_zones.all
+}
 #
 #data "csd_zone" "myzone" {
 #  name = "myzone.idealo.tools"
