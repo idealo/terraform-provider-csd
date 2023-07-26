faef4888caab655a74efe47471d8189cd35575e9
Rocky Wiebeck <rocky.wiebeck@idealo.de>
2023-07-26 16:40:12 +0200

    []

--- examples/main.tf
+++ examples/main.tf
@@ -20,13 +20,13 @@ provider "aws" {
 
 provider "csd" {}
 
-resource "aws_route53_zone" "my_zone_delegation" {
+resource "aws_route53_zone" "my_zone" {
   name = "myzone.idealo.tools"
 }
 
 resource "csd_zone_delegation" "my_zone_delegation" {
-  name = aws_route53_zone.my_zone_delegation.name
-  name_servers = aws_route53_zone.my_zone_delegation.name_servers
+  name = aws_route53_zone.my_zone.name
+  name_servers = aws_route53_zone.my_zone.name_servers
 }
 
 output "test_name" {
